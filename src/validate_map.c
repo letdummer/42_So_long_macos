4088fbe24fe195c67ef66bacc7494ad7c0fd7d5a
Leticia Dummer <ldummer-@student.42porto.com>
2025-06-11 12:11:45 +0100

    fix map validation

--- src/validate_map.c
+++ src/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: ldummer- <ldummer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 21:27:25 by ldummer-          #+#    #+#             */
-/*   Updated: 2025/06/09 14:31:28 by ldummer-         ###   ########.fr       */
+/*   Updated: 2025/06/11 12:08:15 by ldummer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,27 @@
 void	ft_validate_map_extension(char *file)
 {
 	char	*extension;
-
+	char	*basename;
+	
 	if (file == NULL)
 		ft_error_message("Invalid file!");
+	if (file[0] == '.')
+	{
+		if (file[1] == '/')
+			file += 2;
+		else
+		ft_error_message("It's a hidden file, not a map '.ber'!");
+	}
+	basename = ft_strrchr(file, '/');
+	if (basename)
+		basename++;
+	else
+		basename = file;
 	extension = ft_strrchr(file, '.');
 	if (extension == NULL || ft_strcmp(extension, ".ber") != 0)
 		ft_error_message("Invalid file. Must be '.ber");
+	if (extension == basename)
+		ft_error_message("Invalid. It's a hidden file.");
 }
 
 void	ft_validate_map_content(t_game *game)
